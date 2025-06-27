5294df5803048f07114d0bfd8994f30aae35c605
Nguyen Dat <ntdat04@outlook.com>
2025-06-27 18:59:59 +0700

    Hiển thị dữ liệu trang chủ

--- src/KnowledgeSpace.ViewModels/Contents/KnowledgeBaseQuickVm.cs
+++ src/KnowledgeSpace.ViewModels/Contents/KnowledgeBaseQuickVm.cs
@@ -8,12 +8,19 @@
 
 		public string CategoryName { get; set; }
 
+		public string CategoryAlias { get; set; }
+
+
 		public string Title { get; set; }
 
 		public string SeoAlias { get; set; }
 
 		public string Description { get; set; }
 
-		public int? ViewCount { get; set; }
+		public int? ViewCount { get; set; } = 0;
+
+		public DateTime CreateDate { get; set; }
+
+		public int? NumberOfVotes { get; set; } = 0;
 	}
 }
